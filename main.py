48c6ce59b4cc4ba9945e1b8e622eb53b281de893
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2025-01-07 01:20:50 +0000

    Save Code

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # Start with Python to MongoDB connection
+from pymongo import MongoClient
+
+client = MongoClient("localhost", 27017)
+db = client.bookstore
+books = db.books
+
+for book in books.find():
+    print(f"Book: {book}")
 
