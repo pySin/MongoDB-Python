fd128f82aba1a2a962882e643364d71a676edf16
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2025-01-07 01:31:25 +0000

    Save Code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Start with Python to MongoDB connection
 from pymongo import MongoClient
 
-client = MongoClient("localhost", 27017)
+# client = MongoClient("localhost", 27017)
+client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000")
+
 db = client.bookstore
 books = db.books
 
