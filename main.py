e9d8bd0041fb3ba07835cec131a3bb81e96304c1
Sinan Mehmedov <47303660+pySin@users.noreply.github.com>
2025-01-07 12:30:21 +0000

    Save Code

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ books = db.books
 
 # books.insert_one({"title": "Big Mamma", "author": "Sammy Some", "rating": 4, "genres":
 #                   ["Drama", "Thriller"]})
-books.insert_one({"title": "Small Mamma", "author": "Grande Cheritos", "rating": 6, "genres":
-                 ["Comedy"]})
+# books.insert_one({"title": "Small Mamma", "author": "Grande Cheritos", "rating": 6, "genres":
+#                  ["Comedy"]})
+
+carla_id = books.insert_one({"title": "New Mamma", "author": "Carla Sveda", "rating": 7, "genres":
+                            ["Musical"]}).inserted_id
 
 for book in books.find():
     print(f"Book: {book}")
 
+print(f"ID just inserted: {carla_id}")
